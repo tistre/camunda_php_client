e3eb6d138dc3078d4cd26a3a9ee23dfdfd1ffac0
Tim Strehle <tim.strehle@spiegel.de>
2020-06-11 17:02:59 +0200

    Add helper methods CamundaTopicBag::getByTopicName(), CamundaVariableBag::getValue()

--- src/CamundaTopicBag.php
+++ src/CamundaTopicBag.php
@@ -11,20 +11,47 @@ use ArrayIterator;
  */
 class CamundaTopicBag extends ArrayIterator
 {
+    /**
+     * CamundaTopicBag constructor.
+     * @param CamundaTopic ...$variables
+     */
     public function __construct(CamundaTopic ...$variables)
     {
         parent::__construct($variables);
     }
 
 
+    /**
+     * @return CamundaTopic
+     */
     public function current(): CamundaTopic
     {
         return parent::current();
     }
 
 
+    /**
+     * @param string $offset
+     * @return CamundaTopic
+     */
     public function offsetGet($offset): CamundaTopic
     {
         return parent::offsetGet($offset);
+    }
+
+
+    /**
+     * @param string $topicName
+     * @return CamundaTopic|null
+     */
+    public function getByTopicName(string $topicName): ?CamundaTopic
+    {
+        foreach ($this as $topic) {
+            if ($topic->getTopicName() === $topicName) {
+                return $topic;
+            }
+        }
+
+        return null;
     }
 }
